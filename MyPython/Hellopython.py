8f91e18c9ad60010b65ef76f6fc60c20b7967edf
胡大林 <35485406+DalinVector@users.noreply.github.com>
2019-03-12 21:43:44 +0800

    学习并练习布尔值和布林运算

--- MyPython/Hellopython.py
+++ MyPython/Hellopython.py
@@ -1,1 +1,17 @@
 print('hello python,hello world') #你好蟒蛇，世界 我来啦！
+print(bool())
+print(bool( ))
+print(bool(''))
+print(bool(' '))
+print(bool(0))
+print(bool(None)) #所有数据类型都自带布尔值，数据只有在0，None和空的时候为False
+
+if True and False or False:
+    print('yes')
+else:
+    print('no') #运算优先级由高到低：not>and>or，下同
+    
+if not True and False or False:
+    print('yes')
+else:
+    print('no')
